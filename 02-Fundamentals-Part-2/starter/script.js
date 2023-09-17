3d21aa5d6b41e5d0f46a1f89c154272391ee35cf
SergioTales1 <algumemailai3@gmail.com>
2023-09-17 10:39:48 -0300

    commit

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -37,7 +37,7 @@ function fruitProcessor(apples, oranges, pieces){
 console.log(fruitProcessor(1, 2, 4))
 
  */ 
-/* */
+/* 
 
 const average = (s1, s2, s3)=>{
    let ave = 0
@@ -69,4 +69,94 @@ function checkData(data){
 }
 
 console.log(checkData(2))
+*/
 /* */
+
+// challenge 2
+
+// function calcTip(bill){
+//    let billValue = bill
+//    let tip = (bill)=>{
+//       if(bill >= 50 && bill <= 300){
+//          return bill * 0.15
+//       } else {
+//          return bill * 0.20
+//       }
+//    }
+//    let tipStore = tip(billValue)
+//    return tipStore
+// }
+
+/*
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20
+
+
+let bills = [125, 555, 44]
+let tips = []
+let totals = []
+for(let i = 0; i < bills.length; i++){
+    tips.push(calcTip(bills[i]))
+    totals.push(bills[i] + tips[i])
+}
+
+
+console.log(totals)
+
+*/
+/*
+let mark = {
+   fullName : 'mark',
+   mass : 78,
+   height : 1.69,
+
+   calcBMI(){
+      this.BMI = this.mass / (this.height ** 2)
+      console.log(this.BMI)
+      return this.BMI
+   }
+}
+
+
+let john = {
+   fullName : 'jhon',
+   mass : 92,
+   height : 1.95,
+
+   calcBMI(){
+      this.BMI = this.mass / (this.height ** 2)
+      console.log(this.BMI)
+      return this.BMI
+   }
+}
+console.log(options())
+
+function options(){
+   let result = ((mark.calcBMI()) > (john.calcBMI())) ? `Mark's BMI (${mark.BMI}) is bigger than John's BMI (${john.BMI})` : `John's BMI (${john.BMI}) is bigger than Mark's BMI (${mark.BMI})`
+   return result
+}
+*/
+
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+let tips = []
+let totals = []
+
+for(let i = 0; i < bills.length ; i++){
+   
+   function calcTip(bills) {
+      let result = bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.20
+      return result
+   }
+   tips.push(calcTip(bills[i]))
+   totals.push(tips[i] + bills[i])
+}
+console.log(bills, tips, totals)
+console.log(calcAverage(totals))
+
+function calcAverage(arr){
+   let sum = 0
+   for(let i = 0; i < arr.length; i++){
+      sum += arr[i]
+   }
+   let average = sum / arr.length
+   return average
+}
