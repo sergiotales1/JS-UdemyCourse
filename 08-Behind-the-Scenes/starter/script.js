36c19030f90819a412391b2461589b06208830e4
SergioTales1 <algumemailai3@gmail.com>
2023-09-24 18:23:36 -0300

    this keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   let age = 2037 - birthYear;
 
@@ -12,3 +13,54 @@ function calcAge(birthYear) {
 
 const firstName = 'Sergio';
 calcAge(1991);
+*/
+
+/*
+const jonas = {
+  year: 1991,
+  calcAge() {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+*/
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(2037 - this.year);
+
+    /*
+    solution 1
+    const self = this;
+    const isMillenial = function () {
+      console.log(self);
+      console.log(self.year >= 1981 && self.year <= 1996);
+    };
+
+    */
+    // solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+};
+
+jonas.calcAge();
+
+function addExpr(a, b) {
+  console.log(arguments);
+  return a + b;
+}
+addExpr(2, 5);
