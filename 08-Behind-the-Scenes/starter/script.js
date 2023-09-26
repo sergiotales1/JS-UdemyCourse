690939886152ac50586a8f22c9b6fe95c563ca98
SergioTales1 <algumemailai3@gmail.com>
2023-09-26 16:06:18 -0300

    section finished

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -66,6 +66,7 @@ function addExpr(a, b) {
 addExpr(2, 5);
 */
 
+/*
 const me = {
   name: 'jonas',
   age: 30,
@@ -74,3 +75,49 @@ const me = {
 const friend = me;
 friend.age = 27;
 console.log(me.age);
+
+// here we are just pointing to the same address of object so even if we change only one (me or friend) both will affect the real object that they are pointing at
+
+*/
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+
+console.log(lastName, oldLastName);
+
+// here all works in a very intuitive way, which we are really changing the values into the call stack, but when we talk about where objects behave things change a little
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+// remember that even using const we can update the infos into jessica object because the variable jessica is nothing more than an address to the real object (which is in HEAP)
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['arroz', 'joao'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('sergio');
+jessicaCopy.family.push('lucas');
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
+
+// here we see that we do can clone an object with a new address but only in shallow level, sub objects will
+// both apply to the same value
