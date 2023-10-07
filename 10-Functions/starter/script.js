fb3168a7f4490313d29510440e7831ad9a297527
SergioTales1 <algumemailai3@gmail.com>
2023-10-07 17:28:48 -0300

    section 10 finished

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -211,4 +211,66 @@ console.log(higherRatePT(100));
 }
 console.log(test1);
 
+
+const secureBooking = function(){
+  let passengerCount = 0
+  return function(){
+    passengerCount++
+    console.log(`${passengerCount} passengers`)
+  }
+}
+
+const booker = secureBooking()
+booker()
+booker()
+booker()
+
+// Example 1
+let f;
+
+const g = function () {
+  let a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  let b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+h();
+f();
+
+function boardPassengers(n, wait) {
+  const perGroup = n / 3;
+  
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups with ${perGroup} each`);
+  }, wait * 1000);
+  
+  console.log(`Will start boarding in ${wait} seconds`);
+}
+
+boardPassengers(180, 3);
+
+
+// coding challenge 2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  console.log("I'M OVER!");
+  document.body.addEventListener('click', () => {
+    header.style.color = 'blue';
+  });
+})();
+
 */
