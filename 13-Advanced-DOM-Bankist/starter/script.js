6248e2cece25f98f61b0729e20dc602945eec3b7
SergioTales1 <algumemailai3@gmail.com>
2023-11-05 20:04:41 -0300

    event propagation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -114,3 +114,36 @@ btnScrollTo.addEventListener('click', e => {
   section1.scrollIntoView({ behavior: 'smooth' });
   console.log(window.scrollX, window.scrollY);
 });
+
+/* 
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  e.currentTarget.style.backgroundColor = randomColor();
+  console.log(e.target, e.currentTarget);
+  console.log('LINK');
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(e.target, e.currentTarget);
+  console.log('links');
+  console.log(e.target === this);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log(e.target, e.currentTarget);
+    console.log('NAV');
+  },
+  true
+);
+*/
