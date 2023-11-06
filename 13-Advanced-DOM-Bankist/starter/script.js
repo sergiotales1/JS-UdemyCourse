77e604a1397cf919dec5fe11c1da1fb904af1015
SergioTales1 <algumemailai3@gmail.com>
2023-11-06 12:08:39 -0300

    tabbed content

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,9 +34,84 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+btnScrollTo.addEventListener('click', e => {
+  const s1Coords = section1.getBoundingClientRect();
+  console.log(s1Coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  // Scrolling
+  // window.scrollTo({
+  //   // this works because we are adding the px that the user scrolled + the px that the section1 has to the top (the account always will result into 628y witch is the position of this section)
+  //   left: s1Coords.left + window.pageXOffset,
+  //   top: s1Coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
+  console.log(window.scrollX, window.scrollY);
+});
+
 ////////////////////////////////////////////////////////////////////////
 
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(element => {
+//   element.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+  console.log(clicked);
+
+  // Activate content area
+  tabsContent.forEach(tab =>
+    tab.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${+clicked.dataset.tab}`)
+    .classList.add(`operations__content--active`);
+});
 /*
+const h1 = document.querySelector('h1');
+console.log(h1);
+console.log(h1.childNodes);
+console.log(h1.children);
+console.log(h1.parentNode);
+// h1.closest('header').style.backgroundColor = 'var(--color-primary)';
+
+// h1.parentElement.style.backgroundColor = 'blue';
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) el.style.transform = 'scale(.5)';
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(e.target, e.currentTarget);
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
 // Selecting Elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -94,26 +171,6 @@ console.log(link.href);
 logo.setAttribute('data-version-number', '3.0');
 console.log(logo.dataset.versionNumber);
 */
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
-btnScrollTo.addEventListener('click', e => {
-  const s1Coords = section1.getBoundingClientRect();
-  console.log(s1Coords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  // Scrolling
-  // window.scrollTo({
-  //   // this works because we are adding the px that the user scrolled + the px that the section1 has to the top (the account always will result into 628y witch is the position of this section)
-  //   left: s1Coords.left + window.pageXOffset,
-  //   top: s1Coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
-  section1.scrollIntoView({ behavior: 'smooth' });
-  console.log(window.scrollX, window.scrollY);
-});
 
 /* 
 
