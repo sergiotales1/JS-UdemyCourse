6117868ac69a1e26fddec55312ccfec7a1830177
sergiotales1 <algumemailai3@gmail.com>
2023-11-13 11:48:51 -0300

    __proto__

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,7 +1,34 @@
 'use strict';
 
 const Person = function(firstName, birthYear){
+  this.firstName = firstName
+  this.birthYear = birthYear
 
+  // Never do this
+  // this.calcAge = function(){
+  //   console.log(2037 - this.birthYear)
+  // }
 }
 
-new Person('Jonas', 1991)
+const jonas = new Person('Jonas', 1991)
+console.log(jonas)
+
+console.log(jonas instanceof Person)
+// jonas.calcAge()
+
+// Prototypes
+console.log(Person.prototype)
+
+Person.prototype.calcAge = function(){
+  console.log(2037 - this.birthYear)
+}
+
+jonas.calcAge()
+
+console.log(Person.prototype.isPrototypeOf(jonas))
+console.log(Person.prototype.isPrototypeOf(Person))
+
+Person.prototype.species = 'Homo Sapiens'
+console.log(jonas.species)
+console.log(jonas.hasOwnProperty('birthYear'))
+console.log(jonas.hasOwnProperty('species'))
