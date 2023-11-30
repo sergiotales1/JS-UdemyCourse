8f55dcff5f42263308b49da95a6eba40819b2a56
sergiotales1 <algumemailai3@gmail.com>
2023-11-30 15:49:04 -0300

    coding-challenge-1

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,6 +3,32 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 ///////////////////////////////////////
 
 // const getCountryData = function (country) {
@@ -38,29 +64,8 @@ const countriesContainer = document.querySelector('.countries');
 
 // getCountryData('brazil');
 // getCountryData('usa');
+
 /*
-const renderCountry = function (data, className = '') {
-  const html = `
-        <article class="country ${className}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].name
-            }</p>
-          </div>
-        </article>
-  `;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
 const getCountryAndNeighborhood = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -88,7 +93,72 @@ const getCountryAndNeighborhood = function (country) {
 
 // getCountryAndNeighborhood('brazil');
 getCountryAndNeighborhood('spain');
+
+
+// const request = fetch(`https://restcountries.com/v2/name/portugal`);
+// console.log(request);
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    console.log(response);
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
+
+const getCountryData = function (country) {
+  // fetch(`https://restcountries.com/v2/name/${country}`)
+  //   .then(response => {
+  //     console.log(response);
+  //     if (!response.ok)
+  //       throw new Error(`Country not found (${response.status})`);
+  //     return response.json();
+  //   })
+  getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
+
+      if (!neighbor) throw new Error('No neighbor found!');
+
+      // Country 2
+      return getJSON(
+        `https://restcountries.com/v2/alpha/${neighbor}`,
+        'Country not found'
+      );
+    })
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => renderError('Something went wrong ' + err.message))
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('brasil');
+});
+
+getCountryData('germany');
+
+
 */
 
-const request = fetch(`https://restcountries.com/v2/name/portugal`);
-console.log(request);
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.city === undefined) throw new Error('Place not found');
+      if (data.country === undefined)
+        throw new Error('You exceed the api limit');
+      console.log(`You are in ${data.city}, ${data.country}`);
+      fetch(`https://restcountries.com/v2/name/${data.country}`)
+        .then(response => response.json())
+        .then(data => renderCountry(data[0]))
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
