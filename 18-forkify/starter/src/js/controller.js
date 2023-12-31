2027aec7f403ce870af0ec10abe3716a6940b545
sergiotales1 <algumemailai3@gmail.com>
2023-12-31 14:27:13 -0300

    forkify started

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const url =
+  'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886';
+// 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc13';
+
+const showRecipe = async () => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      publisher: recipe.publisher,
+      ingredients: recipe.ingredients,
+      id: recipe.id,
+      title: recipe.title,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      image: recipe.image,
+      sourceUrl: recipe.image_url,
+    };
+    console.log(recipe);
+
+    console.log(data);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
